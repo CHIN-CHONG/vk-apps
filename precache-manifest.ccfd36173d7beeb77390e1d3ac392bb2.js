e81aa2bdd7cd88fbd07924e7bdb94d3472e35b24
Irishery <63507053+Irishery@users.noreply.github.com>
2020-07-09 13:19:59 +0300

    Updates

---
+++ precache-manifest.ccfd36173d7beeb77390e1d3ac392bb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d2b6b71fff1fb96c7c433d94f29dbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cbb3791a7a414e2cf59",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "3b81a4cbb8f048ae7060",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "6cbb3791a7a414e2cf59",
+    "url": "/static/js/2.ca2dc28c.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.ca2dc28c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b81a4cbb8f048ae7060",
+    "url": "/static/js/main.8faed6af.chunk.js"
+  },
+  {
+    "revision": "1550bc5cb6224d84350f",
+    "url": "/static/js/runtime-main.74ac9606.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
