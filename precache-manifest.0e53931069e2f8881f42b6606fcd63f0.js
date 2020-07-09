686feb4da1034e930a6ee58d9784fee6dccbf3fb
Irishery <63507053+Irishery@users.noreply.github.com>
2020-07-09 13:25:19 +0300

    Updates

---
+++ precache-manifest.0e53931069e2f8881f42b6606fcd63f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bf5676b8a063940f03405db3b9a99e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b65988438788e4ed6fb",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "3b81a4cbb8f048ae7060",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "3b65988438788e4ed6fb",
+    "url": "./static/js/2.14299950.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.14299950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b81a4cbb8f048ae7060",
+    "url": "./static/js/main.8faed6af.chunk.js"
+  },
+  {
+    "revision": "fb54b6bed906a3e09b6a",
+    "url": "./static/js/runtime-main.54d55149.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ccfd36173d7beeb77390e1d3ac392bb2.js"
+  "./precache-manifest.0e53931069e2f8881f42b6606fcd63f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
